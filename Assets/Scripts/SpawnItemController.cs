839ce6cc422153a34fe4a3d7cbdb52db3afbdc22
Ivan Camilo Quintero Santacruz <ivanca2291@hotmail.com>
2022-09-10 16:08:01 -0500

    _Clone tag for Destroy instantiated items

--- Assets/Scripts/SpawnItemController.cs
+++ Assets/Scripts/SpawnItemController.cs
@@ -24,17 +24,16 @@ public class SpawnItemController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.tag == "Player")
         {
             if (spawnOption == SpawnOption.Create)
             {
-                //mySpawnObject = GameObject.Instantiate(gameObjectItem);
-                Instantiate(gameObjectItem, new Vector3(spawnTransform.position.x + spawnOffsetX, spawnTransform.position.y + spawnOffsetY, spawnTransform.position.z), Quaternion.identity);
+                GameObject instantiatedObject = Instantiate(gameObjectItem, new Vector3(spawnTransform.position.x + spawnOffsetX, spawnTransform.position.y + spawnOffsetY, spawnTransform.position.z), Quaternion.identity);
+                instantiatedObject.tag = gameObjectItem.name+"_Clone";
             }
             else if (spawnOption == SpawnOption.Delete)
             {
-                var objects = GameObject.FindGameObjectsWithTag(gameObjectItem.tag);
+                var objects = GameObject.FindGameObjectsWithTag(gameObjectItem.tag == "Untagged" ? gameObjectItem.name+"_Clone": gameObjectItem.tag);
                 Destroy(objects.Length >= 1 ? objects[objects.Length - 1] : null);
             };
         }
